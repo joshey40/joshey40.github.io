168134f55eeb4bb286d135a159d9ee246ae1b029
joshey40 <joshuapieniak@gmail.com>
2024-07-19 15:51:17 +0200

    fetcher script

---
+++ CustomMarvelSnap/index.js
@@ -0,0 +1,7 @@
+
+// Get Cards and Locations from datafetch.js
+import { getCards, getLocations } from './scripts/datafetch.js';
+
+// Get the cards and locations
+var cards = getCards();
+var locations = getLocations();
