714648a474b5390e773fa592eeff4fc9256c7198
joshey40 <joshuapieniak@gmail.com>
2024-08-12 18:06:00 +0200

    test

--- clever/script.js
+++ clever/script.js
@@ -1,7 +1,16 @@
 const dice_colors = ["white", "silver", "yellow", "blue", "green", "pink"];
+const dice_values = {
+    "white": 1,
+    "silver": 1,
+    "yellow": 1,
+    "blue": 1,
+    "green": 1,
+    "pink": 1
+};
 
 function rerollDice() {
     var dice_container = document.getElementById("dice_container");
+    dice_container.innerHTML = "";
     for (var i = 0; i < 6; i++) {
         dice = document.createElement("div");
         dice.className = "dice";
