95ea34b5fa6fcd1529d85df556054b2d5c16b6dd
joshey40 <joshuapieniak@gmail.com>
2025-03-25 11:06:43 +0100

    test

--- CustomMarvelSnap/CardMaker/script.js
+++ CustomMarvelSnap/CardMaker/script.js
@@ -2,7 +2,7 @@ import {generatecard} from '../scripts/cardDesign.js';
 
 function updateResult() {
     const name = document.getElementById('name').value;
-    const colorName = document.getElementById('colorName').value;
+    const colorName = document.getElementById('nameColor').value; // Fixed ID reference
     const cost = document.getElementById('cost').value;
     const power = document.getElementById('power').value;
     const description = document.getElementById('description').value;
@@ -13,5 +13,6 @@ function updateResult() {
     // Update the card image
     const cardImage = document.getElementById('cardImage');
     cardImage.src = canvas.toDataURL();
-
 }
+
+export { updateResult }; // Ensure the function is globally accessible
