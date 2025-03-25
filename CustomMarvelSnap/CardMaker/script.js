31c5bfcb71ac56adf2e4c283343a2efb76aba3f3
joshey40 <joshuapieniak@gmail.com>
2025-03-25 11:21:59 +0100

    fix

--- CustomMarvelSnap/CardMaker/script.js
+++ CustomMarvelSnap/CardMaker/script.js
@@ -1,6 +1,6 @@
 import {generatecard} from '../scripts/cardDesign.js';
 
-function updateResult() {
+async function updateResult() {
     const name = document.getElementById('name').value;
     const colorName = document.getElementById('nameColor').value;
     const cost = document.getElementById('cost').value;
@@ -8,7 +8,7 @@ function updateResult() {
     const description = document.getElementById('description').value;
 
     // Update the card
-    const canvas = generatecard(name, colorName, cost, power, description);
+    const canvas = await generatecard(name, colorName, cost, power, description);
 
     // Update the card image
     const cardImageDiv = document.getElementById('resultDiv');
