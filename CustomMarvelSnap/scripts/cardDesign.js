0d9bb197ad759c7b281dfe430d0a78c335fec092
joshey40 <joshuapieniak@gmail.com>
2025-04-06 18:29:41 +0200

    power/cost alignment

--- CustomMarvelSnap/scripts/cardDesign.js
+++ CustomMarvelSnap/scripts/cardDesign.js
@@ -73,7 +73,7 @@ async function generatecard(name, colorName, cost, power, description, size=1024
         costWidth += numbersWidth[costNumber[i]] * scale;
     }
     let costX = 240 * scale - costWidth / 2; 
-    let costY = 88 * scale;
+    let costY = 80 * scale;
     for (let i = 0; i < costNumber.length; i++) {
         let numberImg = await getImg(numbersDir + "cost/" + costNumber[i] + ".png");
         ctx.drawImage(numberImg, costX, costY, numbersWidth[costNumber[i]] * scale, 79 * scale);
@@ -87,7 +87,7 @@ async function generatecard(name, colorName, cost, power, description, size=1024
             powerWidth += numbersWidth[powerNumber[i]] * scale;
         }
         let powerX = 787 * scale - powerWidth / 2;
-        let powerY = 88 * scale + 79 * scale;
+        let powerY = 80 * scale;
         for (let i = 0; i < powerNumber.length; i++) {
             let numberImg = await getImg(numbersDir + "power/" + powerNumber[i] + ".png");
             ctx.drawImage(numberImg, powerX, powerY, numbersWidth[powerNumber[i]] * scale, 79 * scale);
