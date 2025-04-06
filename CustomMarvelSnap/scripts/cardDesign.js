d812d7be3295ba6ff1801f03ce18a2e84efc41b6
joshey40 <joshuapieniak@gmail.com>
2025-04-06 20:21:27 +0200

    test

--- CustomMarvelSnap/scripts/cardDesign.js
+++ CustomMarvelSnap/scripts/cardDesign.js
@@ -28,8 +28,8 @@ async function generatecard(name, colorName, cost, power, description, size=1024
         w = h * aspectRatio;
     }
     let scale = size / 1024;
-    w *= scale;
-    h *= scale;
+    w *= scale * zoom;
+    h *= scale * zoom;
     let x = (1024 - w) / 2 * scale;
     let y = (1024 - h) / 2 * scale + 3 * scale;
     ctx.drawImage(backgroundImg, x, y, w, h);
