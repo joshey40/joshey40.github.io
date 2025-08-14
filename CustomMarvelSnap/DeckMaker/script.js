b741b65f99f6dbb84735e1ecfb4ac3078d183bd2
joshey40 <joshuapieniak@gmail.com>
2025-08-14 17:51:25 +0200

    test images

--- CustomMarvelSnap/DeckMaker/script.js
+++ CustomMarvelSnap/DeckMaker/script.js
@@ -1,6 +1,16 @@
 import { getOfficialCards } from "../scripts/datafetch.js";
 
-console.log("Card Maker Script Loaded");
 const officialCards = await getOfficialCards();
-console.log(officialCards);
-// get Card by Name
+
+
+for (let i = 0; i < 12; i++) {
+    const card_slot = document.getElementById(`card-slot-${i}`);
+    if (card_slot) {
+        const card = officialCards[i];
+        if (card) {
+            card_slot.innerHTML = `<img src="${card.art}" alt="${card.name}">`;
+        } else {
+            card_slot.innerHTML = `<img src="https://joshey40.github.io/CustomMarvelSnap/res/img/default_cards/empty.png" alt="placeholder">`;
+        }
+    }
+}
