9239fe71b1c3efb3217b53d5848c1cbf151f3455
joshey40 <joshuapieniak@gmail.com>
2025-08-14 16:26:53 +0200

    log

--- CustomMarvelSnap/DeckMaker/script.js
+++ CustomMarvelSnap/DeckMaker/script.js
@@ -1,6 +1,6 @@
 import { getOfficialCards } from "../scripts/datafetch.js";
 
-
+console.log("Card Maker Script Loaded");
 const officialCards = await getOfficialCards();
 console.log(officialCards);
 // get Card by Name
