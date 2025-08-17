5d42b1a4a31f41dd3128826aa1b43dd574716f20
joshey40 <joshuapieniak@gmail.com>
2025-08-17 14:21:44 +0200

    finishes mask fix

--- CustomMarvelSnap/scripts/cardDesign.js
+++ CustomMarvelSnap/scripts/cardDesign.js
@@ -231,13 +231,12 @@ async function generatecard(
     ctx.globalCompositeOperation = "multiply";
     const foilImageMask = await getPreloadedImage("../res/img/finishes/foil.png");
     ctx.drawImage(foilImageMask, 0, 0, size, size);
-    ctx.drawImage(artMask, 0, 0, size, size);
     ctx.globalCompositeOperation = "source-in";
   } else if (finish === "gold") {
     ctx.globalCompositeOperation = "multiply";
     const goldImageMask = await getPreloadedImage("../res/img/finishes/gold.png");
     ctx.drawImage(goldImageMask, 0, 0, size, size);
-    ctx.drawImage(artMask, 0, 0, size, size);
+    ctx.globalCompositeOperation = "source-in";
   }
   // Frame
   let frameImg;
